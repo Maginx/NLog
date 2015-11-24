2a0d9fe2883c153091abe6be31fcc6dd266d36f7
Wang Jeremy <wj982397072@163.com>
2015-11-25 00:13:11 +0800

    NLog demo program

---
+++ Log4net_demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4net_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] temp = new int[10];
+                Logging.Debug("this is a debug log test!");
+                Logging.InfoMessage("This is just a test!");
+                Console.WriteLine(temp[10]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Logging.ErrorMessage(e);
+
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Log4net_demo/logging.cs
@@ -0,0 +1,64 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4net_demo
+{
+    sealed class Logging
+    {
+        /// <summary>
+        /// Log error info instance
+        /// </summary>
+        private static Logger loggerInfo = LogManager.GetLogger("info");
+
+        /// <summary>
+        /// Log error message instance
+        /// </summary>
+        private static Logger loggerError = LogManager.GetLogger("error");
+        
+        private Logging()
+        {
+
+        }
+
+        /// <summary>
+        /// Log info message
+        /// </summary>
+        /// <param name="message">message content,string</param>
+        public static void InfoMessage(string message)
+        {
+            loggerInfo.Info(message);
+        }
+
+        /// <summary>
+        /// Log error message to errorlogs
+        /// </summary>
+        /// <param name="e">Excetpion instance for trace stack information</param>
+        public static void ErrorMessage(Exception e)
+        {
+            loggerError.Error(e);
+        }
+        
+        /// <summary>
+        /// Log error message to errorlogs
+        /// </summary>
+        /// <param name="message">message content,string</param>
+        /// <param name="e">exception instance for trace stack</param>
+        public static void ErrorMessage(string message, Exception e)
+        {
+            loggerError.Error(e, message);
+        }
+
+        /// <summary>
+        /// Debug message only for debug
+        /// </summary>
+        /// <param name="message">message content,string</param>
+        public static void Debug(string message)
+        {
+            loggerInfo.Debug(message);
+        }
+    }
+}
